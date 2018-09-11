92f06317ad19039776784eca66b55e3a9448bad0
c-eng <ceng@cca.edu>
2018-09-11 23:59:10 +0000

    minimizing code

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    print(chr(char), end='')
+print('abcdefghijklmnopqrstuvwxyz', end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for char in range(97, 123):
-    if chr(char) != 'e' and chr(char) != 'q':
-        print(chr(char), end='')
+print('abcdfghijklmnoprstuvwxyz', end='')
