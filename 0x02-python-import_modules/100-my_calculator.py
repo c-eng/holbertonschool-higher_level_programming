03637a17931e075d98ee7730f925b59131b29fc8
c-eng <ceng@cca.edu>
2018-09-13 23:41:19 +0000

    First optional task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("{:s}".format(
+            "Usage: ./100-my_calculator.py <a> <operator> <b>"))
+        exit(1)
+    elif argv[2] == '+':
+        print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2],
+                                             int(argv[3]),
+                                             add(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '-':
+        print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2],
+                                             int(argv[3]),
+                                             sub(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '*':
+        print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2],
+                                             int(argv[3]),
+                                             mul(int(argv[1]), int(argv[3]))))
+    elif argv[2] == '/':
+        print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2],
+                                             int(argv[3]),
+                                             div(int(argv[1]), int(argv[3]))))
+    else:
+        print("{:s}".format(
+            "Unknown operator. Available operators: +, -, * and /"))
+        exit(1)
