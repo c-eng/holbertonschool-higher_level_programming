00094b926b1cacfe675c2eac2aa49baae733e5e0
c-eng <ceng@cca.edu>
2018-09-19 03:49:22 +0000

    slimming down code

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,52 +1,24 @@
 #include "lists.h"
 
 /**
- * _memcpy - copies n bytes from src to dest
- * @dest: bytes copied to
- * @src: bytes copied from
- * @n: number of bytes copied
+ * get_listint_at_index - returns nth node of a list
  *
- * Return: pointer to dest
+ * @head: head of list
+ * @index: index of node to return
+ *
+ * Return: node at nth index or NULL if it does not exist
  */
-
-char *_memcpy(char *dest, char *src, unsigned int n)
+listint_t *get_listint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
-
-	for (i = 0 ; i < n ; i++)
-	{
-		*(dest + i) = *(src + i);
-	}
-
-	return (dest);
-}
-
-/**
- * _realloc - realloc
- *
- * @ptr: memory to be reallocated
- * @size: size of memory of be reallocated
- *
- * Return: pointer to memory or NULL
- */
-
-void *_realloc(void *ptr, size_t size)
-{
-	char *new_ptr = NULL;
-
-	if (!ptr)
-		return (malloc(size));
-	if (!size)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	new_ptr = malloc(size);
-	if (!new_ptr)
-		return (NULL);
-	_memcpy(new_ptr, ptr, size - 4);
-	free(ptr);
-	return (new_ptr);
+        unsigned int idx = 0;
+        while (head && idx < index)
+        {
+                idx++;
+                head = head->next;
+        }
+        if (idx == index && head)
+                return (head);
+        return (NULL);
 }
 
 /**
@@ -61,7 +33,6 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *strider = NULL;
 	size_t idx = 0, adx = 0;
-	int *array = NULL;
 
 	if (!head)
 		return (0);
@@ -70,21 +41,16 @@ int is_palindrome(listint_t **head)
 	strider = *head;
 	while (strider)
 	{
-		array = _realloc(array, sizeof(int) * (idx + 1));
-		if (!array)
-			return (1);
-		array[idx] = strider->n;
 		idx += 1;
 		strider = strider->next;
 	}
 	for ( ; adx <= (idx / 2) ; adx++)
 	{
-		if (!(array[adx] == array[idx - adx - 1]))
+		if (!(get_listint_at_index(*head, adx)->n ==
+		      get_listint_at_index(*head, idx - adx - 1)->n))
 		{
-			free(array);
 			return (0);
 		}
 	}
-	free(array);
 	return (1);
 }
