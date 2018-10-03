7c62e957fe3eb2d6e9cc59d532397863fadfe18a
c-eng <ceng@cca.edu>
2018-10-03 02:57:52 +0000

    Testing more docstrings documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 class Square:
+    """Generic square object
+    """
 
     def __init__(self, size):
+        """Initilization method for the Square class.
+
+        Args:
+            size (int): Size of Square
+
+        """
         self.__size = size
