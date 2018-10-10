00b9d685bca6dfa59858ed807fe76cfe78616950
c-eng <ceng@cca.edu>
2018-10-10 15:58:59 +0000

    Removing documentation to meet requirements

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-"""Module to duplicate a list object
-"""
-
-
 def copy_list(l):
-    """Function to return a copy of the passed list
-
-    Args:
-        l(list): List to be copied
-    """
     return l[:]
