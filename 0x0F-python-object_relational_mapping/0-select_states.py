6546338daa0316e84568e2801d4596218c3ac3c2
c-eng <ceng@cca.edu>
2019-01-03 20:43:49 +0000

    Fixing syntax and variable names

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,10 +3,11 @@
 """
 
 import MySQLdb
+from sys import argv
 db = MySQLdb.connect(user=argv[1], passwd=argv[2], host='localhost',
                      port=3306, db=argv[3])
 cursor = db.cursor()
 cursor.execute("SELECT * FROM states ORDER BY id")
-states = cur.fetchall()
+states = cursor.fetchall()
 for state in states:
     print("{}".format(state))
