e0422babe029518884c6099cf3bd7fe899b6cbb4
c-eng <ceng@cca.edu>
2019-01-04 03:37:53 +0000

    Correcting styling

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Definition of State class
     """
@@ -14,4 +15,4 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True,
                 nullable=False)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
