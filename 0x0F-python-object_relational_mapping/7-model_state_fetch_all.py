e29df5663d94a7aabe760e3449dc5d843ea25fe8
c-eng <ceng@cca.edu>
2019-01-04 04:47:33 +0000

    Adding task

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""list all State objects from hbtn_0e_6_usa
+"""
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    session = sessionmaker(bind=engine)()
+    states = session.query(State).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
