650c808618ce933dc0c1d8fa3071674d52603c02
c-eng <ceng@cca.edu>
2019-01-22 05:10:26 +0000

    updating url

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,13 +9,14 @@ from sys import argv
 if __name__ == "__main__":
     if len(argv) < 2:
         argv.append("")
-    dest = 'http://34.206.234.184:41916/search_user'
+    dest = 'http://0.0.0.0:5000/search_user'
     r = requests.post(dest, data = {'q': argv[1]})
     try:
         r.json()
     except Exception:
         print('Not a valid JSON')
-    if not r.json():
-        print('No result')
     else:
-        print("[{}] {}".format(r.json()['id'], r.json()['name']))
+        if not r.json():
+            print('No result')
+        else:
+            print("[{}] {}".format(r.json()['id'], r.json()['name']))
