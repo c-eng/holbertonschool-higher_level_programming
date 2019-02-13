1396fcf989e6630a9f22f9ae96e555b847654be5
c-eng <ceng@cca.edu>
2019-02-13 03:56:13 +0000

    fix: changing function declaration method

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,12 +4,8 @@ let myObject = {
   value: 12
 };
 console.log(myObject);
-myObject = {
-  type: 'object',
-  value: 12,
-  incr: function () {
-    this.value += 1;
-  }
+myObject.incr = function () {
+  this.value += 1;
 };
 myObject.incr();
 console.log(myObject);
