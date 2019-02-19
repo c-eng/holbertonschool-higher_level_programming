5438e74dd788548ffcd5e8883054c922667cbc52
c-eng <ceng@cca.edu>
2019-02-19 02:44:50 +0000

    feat: Adding Square classes

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/use/bin/node
+const Square0 = require('./5-square');
+module.exports = class Square extends Square0 {
+  charPrint (c) {
+    if (c) {
+      let i = 0;
+      for (; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+};
