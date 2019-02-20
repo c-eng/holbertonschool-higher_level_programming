daee3ee18cbbc524558dd0d6a5cb26937fe9d058
c-eng <ceng@cca.edu>
2019-02-20 01:23:32 +0000

    fix: removing offending newline

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,6 @@ let path = process.argv[2];
 let text = process.argv[3];
 if (!path || !text) {
 } else {
-  text += '\n';
   fs.writeFile(path, text, 'utf-8', function (err) {
     if (err) {
       console.log(err);
